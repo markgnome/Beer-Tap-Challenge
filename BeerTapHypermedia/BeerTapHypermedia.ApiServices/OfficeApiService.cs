faf48d3c8caea7daf57a3415b2622b33dbb40090
Mark Milan <markgnome@gmail.com>
2016-07-11 14:04:10 +0800

    Update Office Api Service
    Update Office Api Service

--- BeerTapHypermedia/BeerTapHypermedia.ApiServices/OfficeApiService.cs
+++ BeerTapHypermedia/BeerTapHypermedia.ApiServices/OfficeApiService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -26,26 +27,38 @@ namespace BeerTapHypermedia.ApiServices
 
         public Task<Office> GetAsync(string id, IRequestContext context, CancellationToken cancellation)
         {
-            return Task.FromResult(new Office()
-            {
-                Description = "Office from Manila",
-                Id = "1",
-                Location = OfficeLocation.Manila,
-                Name = "Nimbyx",
-                Kegs = new List<Keg>()
-                {
-                    new Keg()
-                    {
-                        Id = "1",
-                        KegState = KegState.Full
-                    }
-                }
-            });
+            var offices = GetManyAsync(context, cancellation).Result;
+            var office = offices.FirstOrDefault(o => o.Id == id);
+            return Task.FromResult(office);
         }
 
         public Task<IEnumerable<Office>> GetManyAsync(IRequestContext context, CancellationToken cancellation)
         {
             throw new NotImplementedException();
+            var offices = new List<Office>();
+
+            foreach (OfficeLocation location in Enum.GetValues(typeof(OfficeLocation)))
+            {
+                var office = new Office
+                {
+                    Description = $"iQ Office {location}",
+                    Id = ((int)location).ToString(),
+                    Location = location,
+                    Name = $"iQ {location}",
+                    Kegs = new List<Keg>
+                    {
+                        new Keg
+                        {
+                            Id = "1",
+                            Quantity = 18927,
+                            Brand = KegBrand.BudLight,
+                        }
+                    }
+                };
+                offices.Add(office);    
+            }
+            IEnumerable<Office> iOffices = offices;
+            return Task.FromResult(iOffices);
         }
 
         public Task<ResourceCreationResult<Office, string>> CreateAsync(Office resource, IRequestContext context, CancellationToken cancellation)
